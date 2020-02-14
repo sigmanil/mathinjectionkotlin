aea55c92b7baaf1869faa11039d8ae2012938148
Sigmund Marius Nilssen <smn@knowit.no>
2020-02-14 10:39:32 +0100

    More multiplication tests

--- src/test/kotlin/smn/mathinjectionkotlin/service/MultiplicationServiceImplTest.kt
+++ src/test/kotlin/smn/mathinjectionkotlin/service/MultiplicationServiceImplTest.kt
@@ -18,6 +18,28 @@ internal class MultiplicationServiceImplTest {
     }
 
     @Test
+    fun testThatNormalProductWorks() {
+        val factor1 = 2
+        val factor2 = 4
+        val expected = factor1 * factor2
+
+        val slot1 = slot<Int>()
+        val slot2 = slot<Int>()
+        every {
+            testRegistry.additionService.sum(capture(slot1), capture(slot2))
+        } answers {
+            slot1.captured + slot2.captured
+        }
+
+        val actual = testRegistry.multiplicationService.product(factor1, factor2)
+        assertEquals(expected, actual)
+
+        verify {
+            testRegistry.additionService.sum(any(), any())
+        }
+    }
+
+    @Test
     fun testThatNegativeProductWorks() {
         val factor1 = -2
         val factor2 = 4
@@ -38,6 +60,7 @@ internal class MultiplicationServiceImplTest {
 
         verify {
             testRegistry.additionService.sum(any(), any())
+            testRegistry.minusService.minus(any())
         }
     }
 }
